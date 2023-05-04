7dab58e403c8b1285ad124119258775c02ca11d0
Márcio Alexandre <marcinhoale123@gmail.com>
2023-05-03 22:14:21 -0300

    finalizado o include

--- projetoguto/index.php
+++ projetoguto/index.php
@@ -56,12 +56,7 @@ include "menu.php";
     </div>  -->
 
     <?php
-    $servidor_bd = "127.0.0.1";
-    $usuario_bd = "root";
-    $senha_bd = "";
-    $banco_de_dados = "delivery_jogos_novo";
-
-    $conexao = mysqli_connect($servidor_bd, $usuario_bd, $senha_bd, $banco_de_dados);
+    include "conexao.php";
 
     $sql_buscar = "select * from jogo";
 
--- projetoguto/login.php
+++ projetoguto/login.php
@@ -10,12 +10,7 @@
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
-$servidor_bd = "127.0.0.1";
-$usuario_bd = "root";
-$senha_bd = "";
-$banco_de_dados = "delivery_jogos_novo";
-
-$conexao = mysqli_connect($servidor_bd, $usuario_bd, $senha_bd, $banco_de_dados);
+include "conexao.php";
 
 $sql_buscar_usuario = "select * from usuario where email = '$email' and senha = '$senha'"; // mudar
 
