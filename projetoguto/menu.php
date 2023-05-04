5fcb768778ff11711263a664d85c293e52e90842
Márcio Alexandre <marcinhoale123@gmail.com>
2023-05-04 20:15:23 -0300

    mensagem de cadastro de cliente

--- projetoguto/menu.php
+++ projetoguto/menu.php
@@ -10,10 +10,17 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li> -->
       </ul>
+      <?php
+      $mensagem = $_GET["msg"] ?? "";
+      if($mensagem == "erro"){
+        echo "<em class='text-danger'> Usuário ou senha inválido.</em> ";
+      }
+      ?>
       <form action="login.php" method="post" class="d-flex">
         <input class="form-control me-2" type="email" name="email" placeholder="E-mail">
         <input class="form-control me-2" type="password" name="senha" placeholder="Senha">
         <button class="btn btn-outline-success me-2" type="submit">Entrar</button>
+
         <a href="novo-usuario.php" class="btn btn-outline-primary">Cadastrar</a>
       </form>
     </div>
--- projetoguto/novo-usuario.php
+++ projetoguto/novo-usuario.php
@@ -14,7 +14,20 @@ include "menu.php";
                 <input name="nome" required placeholder="Nome"><br>
                 <input name="email" type="email" required placeholder="E-mail"><br>
                 <input name="senha" type="password" required placeholder="Senha"><br>
-                <button type="submit" class="btn btn-success">salvar usuário</button>
+
+                <button type="submit" class="btn btn-success mt-3">salvar usuário</button>
+
+
+                <div class="row">
+                    <div class="col">
+                        <?php
+                        $mensagem = $_GET["msg"] ?? "";
+                        if ($mensagem == "sucesso") {
+                            echo "<div class='alert alert-success col-3 mt-3' role='alert'> Usuário cadastrado com sucesso!</div> ";
+                        }
+                        ?>
+                    </div>
+                </div>
 
             </form>
 
