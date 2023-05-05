d7312facb3c1d9fef4a03b710fcc1e374973da1b
Márcio Alexandre <marcinhoale123@gmail.com>
2023-05-04 21:56:21 -0300

    lista de usuários

---
+++ projetoguto/listar-jogos.php
@@ -0,0 +1,40 @@
+<?php
+include "cabecalho.php";
+include "menu-sistema.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Lista de jogos</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <table border="1">
+                <tr>
+                    <td>Códigos</td>
+                    <td>Título</td>
+                    <td>Categorias</td>
+                    <td>Ações</td>
+                </tr>
+                <?php
+                include "conexao.php";
+                $sql_buscar = "select * from jogo";
+                $todos_os_jogos = mysqli_query($conexao, $sql_buscar);
+                while ($um_jogo = mysqli_fetch_assoc($todos_os_jogos)):
+                    ?>
+                    <tr>
+                        
+                    <td> <?php echo $um_jogo["id"]; ?> </td>
+                    <td> <?php echo $um_jogo["titulo"]; ?> </td>
+                    <td> <?php echo $um_jogo["categoria"]; ?> </td>
+                    </tr>
+                    <?php
+                endwhile;
+                mysqli_close($conexao);
+                ?>
+            </table>
+        </div>
+    </div>
+</div>
